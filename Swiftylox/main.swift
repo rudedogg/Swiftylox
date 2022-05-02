87176a2f6f418574f95a7cafa120750e71483706
Austin Rude <rudedogg@users.noreply.github.com>
2022-05-01 23:57:17 -0600

    Add basic argument handling

--- Swiftylox/main.swift
+++ Swiftylox/main.swift
@@ -1,11 +1,32 @@
-//
-//  main.swift
-//  Swiftylox
-//
-//  Created by Austin Rude on 5/1/22.
-//
-
 import Foundation
 
-print("Hello, World!")
+switch CommandLine.arguments.count {
+  case 1:
+    guard let fileArgument = CommandLine.arguments.first else {
+      print("First argument is invalid")
+      exit(64)
+    }
+    
+    guard let fileURL = URL(string: fileArgument) else {
+      print("Provided file argument is not valid")
+      exit(64)
+    }
+    
+    // We have a valid fileURL, call runFile() with it
+    runFile(fileURL)
+  case 0:
+    // No file path provided, open an interactive interpreter
+    runPrompt()
+  default:
+  // Invalid number of arguments
+  print("Usage: swiftylox [script]")
+  exit(64)
+}
 
+func runFile(_ url: URL) {
+  
+}
+
+func runPrompt() {
+  
+}
