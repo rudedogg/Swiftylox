7a90df3417ed544849a36bcfa7f30c8bdef4ba49
Austin Rude <rudedogg@users.noreply.github.com>
2022-05-03 23:53:37 -0600

    Add the Token class

---
+++ Swiftylox/Token.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+class Token {
+  let type: TokenType
+  let lexeme: String
+  let literal: Any?
+  let line: Int
+  
+  init(type: TokenType, lexeme: String, literal: Any?, line: Int) {
+    self.type = type
+    self.lexeme = lexeme
+    self.literal = literal
+    self.line = line
+  }
+}
+
+
